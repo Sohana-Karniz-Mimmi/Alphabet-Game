3ac7f633acebfddac81da6054e28565573d3fca5
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-02-14 13:52:47 +0600

    home screen js complet

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -1,4 +1,13 @@
 // home screen
 function playBtn(){
-
+    // Hidden Home Screen
+   const homeScreen = document.getElementById('home');
+   homeScreen.classList.add('hidden');
+   
+    //Add Game Screen to Home 
+    const gameScreen = document.getElementById('game-screen');
+    gameScreen.classList.remove('hidden');
 }
+
+
+// Game Screen
